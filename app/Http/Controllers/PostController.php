d46a083bf01085fd42d05c8480426f5bb58c10b1
Hiro-5lab <hiromu.50505002@gmailcom>
2024-12-19 08:34:36 +0000

    er

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,13 +12,4 @@ class PostController extends Controller
         return view('posts.index')->with(['posts' => $post->get()]);
         //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
-    public function getByLimit(int $limit_count = 10)
-    {
-        // updated_atで降順に並べたあと、limitで件数制限をかける
-        return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
-    }
-    public function index(Post $post)
-    {
-        return view('posts.index')->with(['posts' => $post->getByLimit()]);
-    }
 }
